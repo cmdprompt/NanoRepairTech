81b3df6991fea4270e56733b67d43d04c644bb64
The Juggernaut <isaac@unl.edu>
2021-07-30 16:39:46 -0500

    wip

---
+++ 1.3/Surgery/Comp_UseScar.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace Ogre.NanoRepairTech
+{
+	public class CompUseEffect_NanoTechHealScar : CompUseEffect
+	{
+		public override void DoEffect(Pawn usedBy)
+		{
+			if (usedBy != null && !usedBy.Dead && usedBy.health != null && usedBy.health.hediffSet != null && usedBy.health.hediffSet.hediffs != null)
+			{
+				List<Hediff> heDiffs = new List<Hediff>(usedBy.health.hediffSet.hediffs);
+				List<Hediff> permanent = new List<Hediff>();
+
+				Dictionary<string, Hediff_NanoTechHealScar> existing = new Dictionary<string, Hediff_NanoTechHealScar>();
+
+				for (int i = 0; i < heDiffs.Count; i++)
+				{
+					Hediff_Injury inj = heDiffs[i] as Hediff_Injury;
+					if (inj != null && inj.IsPermanent())
+					{
+						permanent.Add(heDiffs[i]);
+						continue;
+					}
+
+					Hediff_NanoTechHealScar healing = heDiffs[i] as Hediff_NanoTechHealScar;
+					if (healing != null)
+					{
+						existing.Add(healing.healingID, healing);
+					}
+				}
+
+				for (int i = 0; i < permanent.Count; i++)
+				{
+					if (!existing.ContainsKey(permanent[i].GetUniqueLoadID()))
+					{
+						//HediffMaker.MakeHediff()
+						HediffDef d = DefDatabase<HediffDef>.AllDefsListForReading.First<HediffDef>(x => x.defName == "Ogre_NanoTech_HeDiffHealScar");
+						Hediff_NanoTechHealScar df = (Hediff_NanoTechHealScar)HediffMaker.MakeHediff(d, usedBy, null);
+						//60,000 ticks per day
+						// 1 severity per day
+						int healAfter = (int)(permanent[i].Severity * 60000);
+						df.ticksTotal = healAfter < 60000 ? 60000 : healAfter;
+
+						df.healingID = permanent[i].GetUniqueLoadID();
+						usedBy.health.AddHediff(df);
+					}
+				}
+			}
+
+			base.DoEffect(usedBy);
+		}
+	}
+}
---
+++ 1.3/Surgery/Hediff_NanoTechHealScar.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Ogre.NanoRepairTech
+{
+	public class Hediff_NanoTechHealScar : HediffWithComps
+	{
+		public int ticksTotal = 0;
+		public string healingID = string.Empty;
+
+		private Hediff_Injury scar = null;
+
+		private Hediff_Injury getScar()
+		{
+			if (scar == null)
+			{
+				List<Hediff> hediffs = new List<Hediff>(this.pawn.health.hediffSet.hediffs);
+				for (int i = 0; i < hediffs.Count; i++)
+				{
+					if (hediffs[i].GetUniqueLoadID() == healingID)
+					{
+						scar = hediffs[i] as Hediff_Injury;
+					}
+				}
+			}
+			return scar;
+		}
+
+		public Hediff_NanoTechHealScar() {
+			
+		}
+
+		public override void Tick()
+		{
+			--ticksTotal;
+			if (ticksTotal % 2500 == 0)
+			{
+				this.getScar().Severity -= 0.1f;
+			}
+
+			if (ticksTotal <= 0)
+			{
+				Verse.Log.Message("Remove.");
+				this.remove();
+			}
+		}
+		
+		private void remove()
+		{
+			Hediff_Injury scar = this.getScar();
+			if (scar != null)
+			{
+				this.pawn.health.RemoveHediff(scar);
+				
+			}
+
+			this.pawn.health.RemoveHediff(this);
+		}
+
+		public override void ExposeData()
+		{
+			Scribe_Values.Look<int>(ref this.ticksTotal, "ticksTotal", 0, true);
+			Scribe_Values.Look<string>(ref this.healingID, "healingID", string.Empty, true);
+			base.ExposeData();
+		}
+	}
+}
